1a6e527ecf7b6a606d650cdb39d3236b8315df3c
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2022-09-13 00:42:50 +0900

    change to slide list item

--- lib/receive/views/receive_list_item.dart
+++ lib/receive/views/receive_list_item.dart
@@ -1,5 +1,6 @@
 // 受信リストアイテム
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 // 受信リストアイテムウィジェットクラス
 class ReceiveListItem extends StatelessWidget{
@@ -39,86 +40,194 @@ class ReceiveListItem extends StatelessWidget{
   @override
   Widget build(BuildContext context)
   {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2),
-      child: ElevatedButton(
-        onPressed: ()=>{},
-        style: ElevatedButton.styleFrom(
-          backgroundColor: _itemBackgroundColor,
-          foregroundColor: _itemForegroundColor,
-          elevation: 5,
-          shape: const ContinuousRectangleBorder(),
-          // side: const BorderSide(
-          //   color: Colors.black,
-          // ),
+    return Slidable(
+      child: IntrinsicHeight(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  // アイコン
+                  Icon(iconData,
+                    color: _itemForegroundColor,
+                    size: _iconSize,
+                  ),
+                  // ファイル名
+                  Flexible(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                      child: Text(
+                        name,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: _fileNameTextStyle,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: _sizeAndSenderWidth,
+              child: Column(
+                children: <Widget>[
+                  // ファイルサイズ
+                  Flexible(
+                    flex: 5,
+                    child: Container(
+                      padding: const EdgeInsets.only(bottom: 2),
+                      alignment: Alignment.bottomRight,
+                      child: Text(size.toString() + 'b',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.right,
+                        style: _sizeTextStyle,
+                      ),
+                    ),
+                  ),
+                  // 送信者
+                  Flexible(
+                    flex: 5,
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 2),
+                      alignment: Alignment.topRight,
+                      child: Text(sender,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.right,
+                        style: _senderTextStyle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        child: IntrinsicHeight(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    // アイコン
-                    Icon(iconData,
-                      color: _itemForegroundColor,
-                      size: _iconSize,
-                    ),
-                    // ファイル名
-                    Flexible(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                        child: Text(
-                          name,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: _fileNameTextStyle,
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: _sizeAndSenderWidth,
-                child: Column(
-                  children: <Widget>[
-                    // ファイルサイズ
-                    Flexible(
-                      flex: 5,
-                      child: Container(
-                        padding: const EdgeInsets.only(bottom: 2),
-                        alignment: Alignment.bottomRight,
-                        child: Text(size.toString() + 'b',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.right,
-                          style: _sizeTextStyle,
-                        ),
-                      ),
-                    ),
-                    // 送信者
-                    Flexible(
-                      flex: 5,
-                      child: Container(
-                        padding: const EdgeInsets.only(top: 2),
-                        alignment: Alignment.topRight,
-                        child: Text(sender,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.right,
-                          style: _senderTextStyle,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+      ),
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            flex: 4,
+            onPressed: (BuildContext context) => {},
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
           ),
-        )
-      )
+        ],
+      ),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            flex: 2,
+            onPressed: (BuildContext context) => {},
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+            icon: Icons.save,
+            label: 'Save',
+          ),
+          SlidableAction(
+            flex: 2,
+            onPressed: (BuildContext context) => {},
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            icon: Icons.more,
+            label: 'More',
+          ),
+        ],
+      ),
     );
   }
+//  @override
+//  Widget build(BuildContext context)
+//  {
+//    return Padding(
+//      padding: const EdgeInsets.symmetric(vertical: 2),
+//      child: ElevatedButton(
+//        onPressed: ()=>{},
+//        style: ElevatedButton.styleFrom(
+//          backgroundColor: _itemBackgroundColor,
+//          foregroundColor: _itemForegroundColor,
+//          elevation: 5,
+//          shape: const ContinuousRectangleBorder(),
+//          // side: const BorderSide(
+//          //   color: Colors.black,
+//          // ),
+//        ),
+//        child: IntrinsicHeight(
+//          child: Row(
+//            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//            children: [
+//              Expanded(
+//                child: Row(
+//                  children: [
+//                    // アイコン
+//                    Icon(iconData,
+//                      color: _itemForegroundColor,
+//                      size: _iconSize,
+//                    ),
+//                    // ファイル名
+//                    Flexible(
+//                      child: Container(
+//                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+//                        child: Text(
+//                          name,
+//                          maxLines: 1,
+//                          overflow: TextOverflow.ellipsis,
+//                          style: _fileNameTextStyle,
+//                          textAlign: TextAlign.left,
+//                        ),
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+//              SizedBox(
+//                width: _sizeAndSenderWidth,
+//                child: Column(
+//                  children: <Widget>[
+//                    // ファイルサイズ
+//                    Flexible(
+//                      flex: 5,
+//                      child: Container(
+//                        padding: const EdgeInsets.only(bottom: 2),
+//                        alignment: Alignment.bottomRight,
+//                        child: Text(size.toString() + 'b',
+//                          maxLines: 1,
+//                          overflow: TextOverflow.ellipsis,
+//                          textAlign: TextAlign.right,
+//                          style: _sizeTextStyle,
+//                        ),
+//                      ),
+//                    ),
+//                    // 送信者
+//                    Flexible(
+//                      flex: 5,
+//                      child: Container(
+//                        padding: const EdgeInsets.only(top: 2),
+//                        alignment: Alignment.topRight,
+//                        child: Text(sender,
+//                          maxLines: 1,
+//                          overflow: TextOverflow.ellipsis,
+//                          textAlign: TextAlign.right,
+//                          style: _senderTextStyle,
+//                        ),
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+//            ],
+//          ),
+//        )
+//      )
+//    );
+//  }
 }
