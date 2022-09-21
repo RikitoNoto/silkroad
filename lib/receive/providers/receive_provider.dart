0c3f84919bf75fa9ffb8eb31e38345a165990eb2
R.Noto <rikitonoto@gmail.com>
2022-09-21 22:42:48 +0900

    create receive test base with mock

--- lib/receive/providers/receive_provider.dart
+++ lib/receive/providers/receive_provider.dart
@@ -1,4 +1,22 @@
 import 'package:flutter/foundation.dart';
+import 'package:network_info_plus/network_info_plus.dart';
 
 class ReceiveProvider with ChangeNotifier {
+  static NetworkInfo networkInfo = NetworkInfo();
+
+  ReceiveProvider()
+  {
+    setIpaddress();
+  }
+
+  void setIpaddress() async
+  {
+    _ipaddress = await networkInfo.getWifiIP();
+  }
+
+  String? _ipaddress;
+
+  String? get ipaddress{
+    return _ipaddress;
+  }
 }
--- test/receive/providers/receive_providers_test.dart
+++ test/receive/providers/receive_providers_test.dart
@@ -7,13 +7,35 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:network_info_plus/network_info_plus.dart';
 import 'package:silkroad/receive/providers/receive_provider.dart';
+import 'receive_providers_test.mocks.dart';
 
+@GenerateMocks([NetworkInfo])
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  MockNetworkInfo networkInfoMock = MockNetworkInfo();
+  ReceiveProvider.networkInfo = networkInfoMock;
+
   group('receive provider tests', () {
-    test('should be initialize and get ip address', (){
-      
+    // test('should be initialize and get ip address (255.255.255.255)', (){
+    //   ReceiveProvider provider = ReceiveProvider(ip: "255.255.255.255");
+    //   expect(provider.ipaddress, "255.255.255.255");
+    // });
+    //
+    // test('should be initialize and get ip address (0.0.0.0)', (){
+    //   ReceiveProvider provider = ReceiveProvider(ip: "0.0.0.0");
+    //   expect(provider.ipaddress, "0.0.0.0");
+    // });
+
+    test('should be get the ip address when initialize(0.0.0.0)', () async {
+      when(networkInfoMock.getWifiIP()).thenAnswer((_)=>Future<String>.value("0.0.0.0"));
+      ReceiveProvider provider = ReceiveProvider();
+      expect(provider.ipaddress, "0.0.0.0");
     });
+
   });
 }
