d08934c886cada1d524b485bf65c69cac6814b8b
R.Noto <rikitonoto@gmail.com>
2022-09-25 09:34:31 +0900

    create sendfile class and none class

--- lib/comm/message.dart
+++ lib/comm/message.dart
@@ -7,35 +7,59 @@ enum Command{
   sendFile,
 }
 
+typedef _MessageFactoryMethod = Message Function(Uint8List data);
+Map<Command, _MessageFactoryMethod> _commandToClassTable = {
+  Command.none    : None.construct,
+  Command.sendFile: SendFile.construct,
+};
+
 abstract class Message{
   const Message({required this.command});
+
   final Command command;
 
   static Map<String, Command> commandConvertTable = {
     'SEND_FILE' : Command.sendFile,
   };
 
-  static Map<Command, Type> _commandToClassTable = {
-    Command.sendFile: Message,
-  };
-
   static Message convert(Uint8List data){
     Command command = commandConvertTable[String.fromCharCodes(data)] != null ?  commandConvertTable[String.fromCharCodes(data)]! : Command.none;
 //    return Message(command: command);
-    if(_commandToClassTable[command] != null){
-      return _commandToClassTable[command]();
+
+    if(_commandToClassTable[command] == null){
+      return _commandToClassTable[Command.none]!(data);
     }
 
-    return _commandToClassTable[command]();
+    return _commandToClassTable[command]!(data);
   }
 
 }
 
-class SendFile implements Message{
+class None implements Message{
+  const None({required this.receiveData}) : this.command = Command.none;
+
   @override
-  Command get command => command;
+  final Command command;
 
-  static Message build(Uint8List data){
+  final Uint8List receiveData;
 
+  static Message construct(Uint8List data){
+    return None(receiveData: data);
+  }
+}
+
+class SendFile implements Message{
+  SendFile({required this.receiveData}) : this.command = Command.sendFile
+  {
+
+  }
+
+  @override
+  final Command command;
+
+  final Uint8List receiveData;
+
+  static Message construct(Uint8List data){
+    return SendFile(receiveData: data);
   }
 }
