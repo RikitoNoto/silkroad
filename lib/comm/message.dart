60eea58469dbb3f8f16b134fbebf9a4a91291843
R.Noto <rikitonoto@gmail.com>
2022-09-25 10:52:41 +0900

    create command analyze process for file send

--- lib/comm/message.dart
+++ lib/comm/message.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:silkroad/comm/comm.dart';
@@ -22,9 +23,13 @@ abstract class Message{
     'SEND_FILE' : Command.sendFile,
   };
 
+  String getDataStr(int index);
+  Uint8List getDataBin(int index);
+
   static Message convert(Uint8List data){
-    Command command = commandConvertTable[String.fromCharCodes(data)] != null ?  commandConvertTable[String.fromCharCodes(data)]! : Command.none;
-//    return Message(command: command);
+    String? commandStr = RegExp('^(.*)\n').firstMatch(String.fromCharCodes(data))?.group(1);
+
+    Command command = commandConvertTable[commandStr] != null ?  commandConvertTable[commandStr]! : Command.none;
 
     if(_commandToClassTable[command] == null){
       return _commandToClassTable[Command.none]!(data);
@@ -46,20 +51,69 @@ class None implements Message{
   static Message construct(Uint8List data){
     return None(receiveData: data);
   }
+
+  @override
+  String getDataStr(int index) {
+    throw ArgumentError("None class can't get data.");
+  }
+
+  @override
+  Uint8List getDataBin(int index) {
+    throw ArgumentError("None class can't get data.");
+  }
 }
 
 class SendFile implements Message{
+  static const int dataIndexName = 0;
+  static const int dataIndexFile = 1;
+
   SendFile({required this.receiveData}) : this.command = Command.sendFile
   {
-
+    name = _fetchName(receiveData);
+    fileData = _fetchFileData(receiveData);
   }
 
   @override
   final Command command;
 
+  late final String name;
+  late final Uint8List fileData;
   final Uint8List receiveData;
 
   static Message construct(Uint8List data){
     return SendFile(receiveData: data);
+  }
+
+  @override
+  String getDataStr(int index) {
+    return name;
+  }
+
+  @override
+  Uint8List getDataBin(int index) {
+    return fileData;
+  }
+
+  static String _fetchName(Uint8List data){
+    String name;
+    String? match = RegExp('^name:(.*)', multiLine: true).firstMatch(String.fromCharCodes(data))?.group(1);
+    if(match != null){
+      name = match;
+    }else{
+      name = '';
+    }
+    return name;
+  }
+
+  static Uint8List _fetchFileData(Uint8List data){
+    Uint8List fileData;
+    String? match = RegExp('^file:(.*)', dotAll: true, multiLine: true).firstMatch(String.fromCharCodes(data))?.group(1);
+
+    if(match != null){
+      fileData = Uint8List.fromList(utf8.encode(match));
+    }else{
+      fileData = Uint8List.fromList(utf8.encode(''));
+    }
+    return fileData;
   }
 }
--- test/comm/message_test.dart
+++ test/comm/message_test.dart
@@ -10,7 +10,11 @@ void main() {
 }
 
 Uint8List convertCommand({command=''}){
-  return Uint8List.fromList(utf8.encode(command));
+  return Uint8List.fromList(utf8.encode(command + '\n'));
+}
+
+Uint8List convertSendFileCommand({name='', file=''}){
+  return Uint8List.fromList(utf8.encode('SEND_FILE\nname:${name}\nfile:${file}'));
 }
 
 
@@ -22,13 +26,43 @@ void commandConvertTest() {
     });
 
     test('should be return sendfile command when receive send file string', () async {
-      Message message = Message.convert(convertCommand(command: 'SEND_FILE'));
+      Message message = Message.convert(convertSendFileCommand());
       expect(message.command, Command.sendFile);
     });
 
     test('should be return none command when receive send file string', () async {
-      Message message = Message.convert(convertCommand(command: 'SEND_FILE'));
+      Message message = Message.convert(convertSendFileCommand());
       expect(message is SendFile, isTrue);
+    });
+
+    test('should be able to get filename A', () async {
+      Message message = Message.convert(convertSendFileCommand(name: 'A'));
+      expect(message is SendFile, isTrue);
+      expect(message.getDataStr(SendFile.dataIndexName), 'A');
+    });
+
+    test('should be able to get filename B', () async {
+      Message message = Message.convert(convertSendFileCommand(name: 'B'));
+      expect(message is SendFile, isTrue);
+      expect(message.getDataStr(SendFile.dataIndexName), 'B');
+    });
+
+    test('should be able to get data I', () async {
+      Message message = Message.convert(convertSendFileCommand(file: 'I'));
+      expect(message is SendFile, isTrue);
+      expect(String.fromCharCodes(message.getDataBin(SendFile.dataIndexFile)), 'I');
+    });
+
+    test('should be able to get data II include new line', () async {
+      Message message = Message.convert(convertSendFileCommand(file: 'I\nI'));
+      expect(message is SendFile, isTrue);
+      expect(String.fromCharCodes(message.getDataBin(SendFile.dataIndexFile)), 'I\nI');
+    });
+
+    test('should be able to get name A when include two names', () async {
+      Message message = Message.convert(convertSendFileCommand(name: 'A',file: 'name: \'B\''));
+      expect(message is SendFile, isTrue);
+      expect(message.getDataStr(SendFile.dataIndexName), 'A');
     });
   });
 }
