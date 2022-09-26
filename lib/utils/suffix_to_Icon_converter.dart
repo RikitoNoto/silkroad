e5b241b3b426806c02eb4f3b22714b4653013769
R.Noto <rikitonoto@gmail.com>
2022-09-26 23:33:57 +0900

    create icon converter class

---
+++ lib/utils/suffix_to_Icon_converter.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class SuffixToIconConverter{
+
+  static const Map<String, IconData> _convertTable = {
+    /// text
+    'txt'   : Icons.description,
+
+    /// image
+    'jpeg'  : Icons.image,
+    'jpg'   : Icons.image,
+    'png'   : Icons.image,
+    'bmp'   : Icons.image,
+    'tiff'  : Icons.image,
+    'tif'   : Icons.image,
+
+    /// music
+    'mp3'   : Icons.music_note,
+
+    /// movie
+    'mp4'   : Icons.movie,
+
+    /// gif
+    'gif'   : Icons.gif,
+
+    /// pdf
+    'pdf'   : Icons.picture_as_pdf,
+  };
+
+  static IconData? convertIcon(String filename){
+    String? suffix = RegExp('\.([^.]*)\$').firstMatch(filename)?.group(1);
+
+    if(suffix != null){
+      return _convertTable[suffix];
+    }
+
+    return null;
+  }
+}
