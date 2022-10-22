eb041beb435205a94cf32b1b466ee2e7346208a0
R.Noto <rikitonoto@gmail.com>
2022-10-23 00:05:03 +0900

    change home view

--- lib/home/views/home_view.dart
+++ lib/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:silkroad/app_theme.dart';
 import 'package:silkroad/utils/views/theme_input_field.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,67 +18,89 @@ class _HomePageState extends State<HomePage> {
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Home'),
+          title: const Text('SilkRoad'),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                child: SizedBox(
-                  height: 50,
-                  child: ThemeInputField(
-                    labelText: 'Your name',
-                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                  ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              child: SizedBox(
+                height: 50,
+                child: ThemeInputField(
+                  labelText: 'Your name',
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
                 ),
               ),
-              _buildActionSelectButton(
-                context,
-                label: 'Send',
-                svgPath: 'assets/svg_icons/transfer-in.svg',
-                onPressed: () => Navigator.pushNamed(context, '/send'),
+            ),
+
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  _buildActionSelectButton(
+                    context,
+                    label: 'Send',
+                    svgPath: 'assets/svg_icons/transfer-out.svg',
+                    iconColor: AppTheme.appIconColor1,
+                    onPressed: () => Navigator.pushNamed(context, '/send'),
+                  ),
+                  _buildActionSelectButton(
+                    context,
+                    label: 'Receive',
+                    svgPath: 'assets/svg_icons/transfer-in.svg',
+                    iconColor: AppTheme.appIconColor2,
+                    onPressed: () => Navigator.pushNamed(context, '/receive'),
+                  ),
+                ],
               ),
-              _buildActionSelectButton(
-                context,
-                label: 'Receive',
-                svgPath: 'assets/svg_icons/transfer-out.svg',
-                onPressed: () => Navigator.pushNamed(context, '/receive'),
-              ),
-            ]
-          ),
+            ),
+
+          ]
         ),
       ),
     );
   }
 
-  Widget _buildActionSelectButton(BuildContext context, {required String label, required String svgPath, void Function()? onPressed}){
-    return SizedBox(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 50),
-        child: ElevatedButton(
-          key: ValueKey<String>(label),
-          onPressed: onPressed,
-          child: SizedBox(
-            height: 200,
-            // width: 200,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SvgPicture.asset(
-                  svgPath,
-                  height: 90,
-                  color: MediaQuery.platformBrightnessOf(context) == Brightness.dark ? Colors.white : Colors.black,
+
+  Widget _buildActionSelectButton(BuildContext context, {required String label, required String svgPath, required Color iconColor, void Function()? onPressed}){
+    return ElevatedButton(
+      key: ValueKey<String>(label),
+      onPressed: onPressed,
+      style: ButtonStyle(
+        elevation: MaterialStateProperty.all<double>(0.0),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        backgroundColor: MaterialStateProperty.all<Color>(AppTheme.getBackgroundColor(context)),
+      ),
+      child: SizedBox(
+        height: 40,
+        child: IntrinsicHeight(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 35,
+                width: 35,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: iconColor,
                 ),
-                Text(
+                child: SvgPicture.asset(
+                  svgPath,
+                  fit: BoxFit.scaleDown,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(width: 10,),
+              Expanded(
+                child: Text(
                   label,
+                  textAlign: TextAlign.left,
                   style: const TextStyle(
-                    fontSize: 40,
+                    fontSize: 16,
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
