70f62d81817a432875cb643c0c0f9b09cf52c838
R.Noto <rikitonoto@gmail.com>
2022-10-30 22:55:08 +0900

    create list item widget for pc

--- lib/receive/views/receive_list_item.dart
+++ lib/receive/views/receive_list_item.dart
@@ -1,13 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:platform/platform.dart';
 
 import 'package:silkroad/app_theme.dart';
-//TODO: change item slider in pc.
+
 
 ///
 /// receive item builder
 ///
-mixin _ListItemBuilder{
+mixin _ListItemBuilderMobile{
+
+  Widget buildListItemMobile(
+      BuildContext context,
+      {
+        required Widget child,
+        EdgeInsetsGeometry? padding,
+        void Function(BuildContext context)? onDelete,
+        void Function(BuildContext context)? onSave,
+        Color? backgroundColor,
+      })
+  {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+      ),
+      padding: padding ?? EdgeInsets.zero,
+      child: Slidable(
+        startActionPane: _buildStartAction(onDelete),
+        endActionPane: _buildEndAction(onSave),
+        child: child,//_buildCardItem(name: name, size: size, sender: sender, iconData: iconData),
+      ),
+    );
+  }
+
+  static ActionPane _buildStartAction(void Function(BuildContext context)? deleteAction){
+    return ActionPane(
+      motion: const ScrollMotion(),
+      children: [
+        SlidableAction(
+          // An action can be bigger than the others.
+          flex: 4,
+          onPressed: deleteAction,
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.white,
+          icon: Icons.delete,
+          label: 'Delete',
+        ),
+      ],
+    );
+  }
+
+  static ActionPane _buildEndAction(void Function(BuildContext context)? saveAction){
+    return ActionPane(
+      motion: const ScrollMotion(),
+      children: [
+        SlidableAction(
+          // An action can be bigger than the others.
+          flex: 2,
+          onPressed: saveAction,
+          backgroundColor: Colors.green,
+          foregroundColor: Colors.white,
+          icon: Icons.save,
+          label: 'Save',
+        ),
+        // SlidableAction(
+        //   flex: 2,
+        //   onPressed: (BuildContext context) => {},
+        //   backgroundColor: Colors.white,
+        //   foregroundColor: Colors.black,
+        //   icon: Icons.more,
+        //   label: 'More',
+        // ),
+      ],
+    );
+  }
+
+}
+
+mixin _ListItemBuilderPc {
+
+  Widget buildListItemPc(
+      BuildContext context,
+      {
+        required Widget child,
+        EdgeInsetsGeometry? padding,
+        void Function(BuildContext context)? onDelete,
+        void Function(BuildContext context)? onSave,
+      })
+  {
+    return ElevatedButton(
+      onPressed: ()=>{},
+      style: ButtonStyle(
+        elevation: MaterialStateProperty.all<double>(0.0),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        backgroundColor: MaterialStateProperty.all<Color>(AppTheme.getBackgroundColor(context)),
+      ),
+      child: child,
+    );
+  }
+}
+
+abstract class ReceiveListItemBase extends StatelessWidget with _ListItemBuilderPc, _ListItemBuilderMobile{
+  const ReceiveListItemBase({super.key});
   static const double _iconSize = 30.0;                     /// icon size
   static const double _sizeAndSenderWidth = 64.0;           /// size and sender column width
 
@@ -33,25 +127,24 @@ mixin _ListItemBuilder{
   /// text style： file size
   static TextStyle get _sizeTextStyle => _senderTextStyle;
 
-  /// item decoration
-  static BoxDecoration get _decorationItem => BoxDecoration(
-    color: _itemBackgroundColor,
-  );
+  // /// item decoration
+  // static BoxDecoration get _decorationItem => BoxDecoration(
+  //   color: _itemBackgroundColor,
+  // );
 
-  static const EdgeInsetsGeometry _itemPadding = EdgeInsets.only(top: 0);
-
-  static Widget build(
-    BuildContext context,
-    {
-      required index,
-      required iconData,
-      required name,
-      required size,
-      required sender,
-      void Function(BuildContext context)? onDelete,
-      void Function(BuildContext context)? onSave,
-    })
+  Widget _buildListItem(BuildContext context,
+                      {
+                        required Platform platform,
+                        required index,
+                        required iconData,
+                        required name,
+                        required size,
+                        required sender,
+                        void Function(BuildContext context)? onDelete,
+                        void Function(BuildContext context)? onSave,
+                      })
   {
+
     // is light mode.
     if(MediaQuery.platformBrightnessOf(context) == Brightness.light){
       _itemForegroundColor = Theme.of(context).textTheme.bodyText1?.color ?? _itemForegroundColor;
@@ -63,97 +156,39 @@ mixin _ListItemBuilder{
       _itemBackgroundColor = _itemDarkModeColor;
     }
 
-    return Padding(
-      padding: _itemPadding,
-      child: Slidable(
-        startActionPane: _buildStartAction(onDelete),
-        endActionPane: _buildEndAction(onSave),
-        child: _buildCardItem(name: name, size: size, sender: sender, iconData: iconData),
-        // child: Container(
-        //   decoration: _decorationItem,
-        //   child: IntrinsicHeight(
-        //     child: Row(
-        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //       children: [
-        //         Expanded(
-        //           child: Row(
-        //             children: [
-        //               _buildIcon(iconData),     // icon
-        //               _buildFileName(name),     // file name
-        //             ],
-        //           ),
-        //         ),
-        //         SizedBox(
-        //           width: _sizeAndSenderWidth,
-        //           child: Column(
-        //             children: <Widget>[
-        //               _buildFileSize(size),   // file size
-        //               _buildSender(sender),   // sender
-        //             ],
-        //           ),
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
-      ),
-    );
+    switch(platform.operatingSystem){
+      case Platform.iOS:
+      case Platform.android:
+        return buildListItemMobile(
+          context,
+          onSave: onSave,
+          onDelete: onDelete,
+          backgroundColor: _itemBackgroundColor,
+          child: _buildCardItem(context, iconData: iconData, name: name, size: size, sender: sender,),
+        );
+
+      default:
+        return buildListItemPc(
+          context,
+          onSave: onSave,
+          onDelete: onDelete,
+          child: _buildCardItem(context, iconData: iconData, name: name, size: size, sender: sender),
+        );
+    }
   }
 
-  static ActionPane _buildStartAction(void Function(BuildContext context)? deleteAction)
-  {
-    return ActionPane(
-      motion: const ScrollMotion(),
-      children: [
-        SlidableAction(
-          // An action can be bigger than the others.
-          flex: 4,
-          onPressed: deleteAction,
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.white,
-          icon: Icons.delete,
-          label: 'Delete',
-        ),
-      ],
-    );
-  }
-
-  static ActionPane _buildEndAction(void Function(BuildContext context)? saveAction)
-  {
-    return ActionPane(
-      motion: const ScrollMotion(),
-      children: [
-        SlidableAction(
-          // An action can be bigger than the others.
-          flex: 2,
-          onPressed: saveAction,
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.white,
-          icon: Icons.save,
-          label: 'Save',
-        ),
-        // SlidableAction(
-        //   flex: 2,
-        //   onPressed: (BuildContext context) => {},
-        //   backgroundColor: Colors.white,
-        //   foregroundColor: Colors.black,
-        //   icon: Icons.more,
-        //   label: 'More',
-        // ),
-      ],
-    );
-  }
-
-  static Widget _buildCardItem(
+  Widget _buildCardItem(
+    BuildContext context,
     {
       required String name,
       IconData? iconData,
       required String size,
       required String sender,
-    }
-  ){
+    })
+  {
+
     return Container(
-      decoration: _decorationItem,
+      // decoration: _decorationItem,
       child: IntrinsicHeight(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -184,7 +219,7 @@ mixin _ListItemBuilder{
     );
   }
 
-  static Widget _buildIcon(IconData? iconData)
+  Widget _buildIcon(IconData? iconData)
   {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
@@ -204,7 +239,7 @@ mixin _ListItemBuilder{
     );
   }
 
-  static Widget _buildFileName(String name)
+  Widget _buildFileName(String name)
   {
     return Flexible(
       child: Container(
@@ -220,7 +255,7 @@ mixin _ListItemBuilder{
     );
   }
 
-  static Widget _buildFileSize(String size)
+  Widget _buildFileSize(String size)
   {
     return Flexible(
       flex: 5,
@@ -237,7 +272,7 @@ mixin _ListItemBuilder{
     );
   }
 
-  static Widget _buildSender(String sender)
+  Widget _buildSender(String sender)
   {
     return Flexible(
       flex: 5,
@@ -255,11 +290,236 @@ mixin _ListItemBuilder{
   }
 }
 
+// ///
+// /// receive item builder
+// ///
+// mixin _ListItemBuilder{
+//   static const double _iconSize = 30.0;                     /// icon size
+//   static const double _sizeAndSenderWidth = 64.0;           /// size and sender column width
+//
+//   static const Color _itemLightModeColor = Colors.white;      /// white mode item color
+//   static const Color _itemDarkModeColor = Colors.black54;     /// dark mode item color
+//
+//   static Color _itemBackgroundColor = Colors.white;   /// item back ground color
+//   static Color _itemForegroundColor = Colors.black;   /// item font color
+//
+//   /// text style： file name
+//   static TextStyle get _fileNameTextStyle => TextStyle(
+//     color: _itemForegroundColor,
+//     fontSize: 20,
+//     // fontWeight: FontWeight.bold,
+//   );
+//
+//   /// text style： sender
+//   static TextStyle get _senderTextStyle => TextStyle(
+//     color: _itemForegroundColor,
+//     fontSize: 12,
+//   );
+//
+//   /// text style： file size
+//   static TextStyle get _sizeTextStyle => _senderTextStyle;
+//
+//   /// item decoration
+//   static BoxDecoration get _decorationItem => BoxDecoration(
+//     color: _itemBackgroundColor,
+//   );
+//
+//   static const EdgeInsetsGeometry _itemPadding = EdgeInsets.only(top: 0);
+//
+//   static Widget build(
+//     BuildContext context,
+//     {
+//       required index,
+//       required iconData,
+//       required name,
+//       required size,
+//       required sender,
+//       void Function(BuildContext context)? onDelete,
+//       void Function(BuildContext context)? onSave,
+//     })
+//   {
+//     // is light mode.
+//     if(MediaQuery.platformBrightnessOf(context) == Brightness.light){
+//       _itemForegroundColor = Theme.of(context).textTheme.bodyText1?.color ?? _itemForegroundColor;
+//       _itemBackgroundColor = _itemLightModeColor;
+//     }
+//     // is dark mode.
+//     else{
+//       _itemForegroundColor = Theme.of(context).textTheme.bodyText1?.color ?? _itemForegroundColor;
+//       _itemBackgroundColor = _itemDarkModeColor;
+//     }
+//
+//     return Padding(
+//       padding: _itemPadding,
+//       child: Slidable(
+//         startActionPane: _buildStartAction(onDelete),
+//         endActionPane: _buildEndAction(onSave),
+//         child: _buildCardItem(name: name, size: size, sender: sender, iconData: iconData),
+//       ),
+//     );
+//   }
+//
+//   static ActionPane _buildStartAction(void Function(BuildContext context)? deleteAction)
+//   {
+//     return ActionPane(
+//       motion: const ScrollMotion(),
+//       children: [
+//         SlidableAction(
+//           // An action can be bigger than the others.
+//           flex: 4,
+//           onPressed: deleteAction,
+//           backgroundColor: Colors.red,
+//           foregroundColor: Colors.white,
+//           icon: Icons.delete,
+//           label: 'Delete',
+//         ),
+//       ],
+//     );
+//   }
+//
+//   static ActionPane _buildEndAction(void Function(BuildContext context)? saveAction)
+//   {
+//     return ActionPane(
+//       motion: const ScrollMotion(),
+//       children: [
+//         SlidableAction(
+//           // An action can be bigger than the others.
+//           flex: 2,
+//           onPressed: saveAction,
+//           backgroundColor: Colors.green,
+//           foregroundColor: Colors.white,
+//           icon: Icons.save,
+//           label: 'Save',
+//         ),
+//         // SlidableAction(
+//         //   flex: 2,
+//         //   onPressed: (BuildContext context) => {},
+//         //   backgroundColor: Colors.white,
+//         //   foregroundColor: Colors.black,
+//         //   icon: Icons.more,
+//         //   label: 'More',
+//         // ),
+//       ],
+//     );
+//   }
+//
+//   static Widget _buildCardItem(
+//     {
+//       required String name,
+//       IconData? iconData,
+//       required String size,
+//       required String sender,
+//     }
+//   ){
+//     return Container(
+//       decoration: _decorationItem,
+//       child: IntrinsicHeight(
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Expanded(
+//               child: Row(
+//                 children: [
+//                   _buildIcon(iconData),     // icon
+//                   _buildFileName(name),     // file name
+//                 ],
+//               ),
+//             ),
+//             SizedBox(
+//               width: _sizeAndSenderWidth,
+//               child: Padding(
+//                 padding: const EdgeInsets.only(right: 8),
+//                 child: Column(
+//                   children: <Widget>[
+//                     _buildFileSize(size),   // file size
+//                     _buildSender(sender),   // sender
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   static Widget _buildIcon(IconData? iconData)
+//   {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+//       child: Container(
+//         padding: const EdgeInsets.all(5.0),
+//         decoration: const BoxDecoration(
+//           // borderRadius: BorderRadius.circular(100),
+//           shape: BoxShape.circle,
+//           color: AppTheme.appIconColor2,
+//         ),
+//         child: Icon(iconData,
+//           color: Colors.white,
+//           size: _iconSize,
+//
+//         ),
+//       ),
+//     );
+//   }
+//
+//   static Widget _buildFileName(String name)
+//   {
+//     return Flexible(
+//       child: Container(
+//         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+//         child: Text(
+//           name,
+//           maxLines: 1,
+//           overflow: TextOverflow.ellipsis,
+//           style: _fileNameTextStyle,
+//           textAlign: TextAlign.left,
+//         ),
+//       ),
+//     );
+//   }
+//
+//   static Widget _buildFileSize(String size)
+//   {
+//     return Flexible(
+//       flex: 5,
+//       child: Container(
+//         padding: const EdgeInsets.only(bottom: 2),
+//         alignment: Alignment.bottomRight,
+//         child: Text(size.toString(),
+//           maxLines: 1,
+//           overflow: TextOverflow.ellipsis,
+//           textAlign: TextAlign.right,
+//           style: _sizeTextStyle,
+//         ),
+//       ),
+//     );
+//   }
+//
+//   static Widget _buildSender(String sender)
+//   {
+//     return Flexible(
+//       flex: 5,
+//       child: Container(
+//         padding: const EdgeInsets.only(top: 2),
+//         alignment: Alignment.topRight,
+//         child: Text(sender,
+//           maxLines: 1,
+//           overflow: TextOverflow.ellipsis,
+//           textAlign: TextAlign.right,
+//           style: _senderTextStyle,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 ///
 /// Receive list item for add.
 ///
-class ReceiveListItem extends StatelessWidget{
+class ReceiveListItem extends ReceiveListItemBase{
   ReceiveListItem({
+    required this.platform,
     required this.iconData,
     required this.name,
     required this.size,
@@ -279,6 +539,7 @@ class ReceiveListItem extends StatelessWidget{
   final Animation<double> animation;  /// size animation
   final void Function(BuildContext context)? onSave;
   final void Function(BuildContext context)? onDelete;
+  final Platform platform;
 
   final Animatable<Offset> _offsetAnimation = Tween<Offset>(
     end: Offset.zero,
@@ -292,7 +553,17 @@ class ReceiveListItem extends StatelessWidget{
   {
     return SlideTransition(
       position: animation.drive(_offsetAnimation),
-      child: _ListItemBuilder.build(context,index: index, iconData: iconData, name: name, size: size, sender: sender, onSave: onSave, onDelete: onDelete,),
+      child: _buildListItem(
+        context,
+        platform: platform,
+        index: index,
+        iconData: iconData,
+        name: name,
+        size: size,
+        sender: sender,
+        onSave: onSave,
+        onDelete: onDelete,
+      ),
     );
   }
 }
@@ -300,8 +571,9 @@ class ReceiveListItem extends StatelessWidget{
 ///
 /// Receive list item for remove.
 ///
-class ReceiveListItemRemoving extends StatelessWidget{
+class ReceiveListItemRemoving extends ReceiveListItemBase{
   const ReceiveListItemRemoving({
+    required this.platform,
     required this.iconData,
     required this.name,
     required this.size,
@@ -317,6 +589,7 @@ class ReceiveListItemRemoving extends StatelessWidget{
   final String size;                  /// file size
   final String sender;                /// sender name
   final Animation<double> animation;  /// size animation
+  final Platform platform;
 
 
   @override
@@ -324,7 +597,15 @@ class ReceiveListItemRemoving extends StatelessWidget{
   {
     return SizeTransition(
       sizeFactor: animation,
-      child: _ListItemBuilder.build(context, index: index, iconData: iconData, name: name, size: size, sender: sender),
+      child: _buildListItem(
+        context,
+        platform: platform,
+        index: index,
+        iconData: iconData,
+        name: name,
+        size: size,
+        sender: sender
+      ),
     );
   }
 }
